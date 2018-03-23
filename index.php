1cabd9db65adfbcb4e6f6da6b6fccd8c795852a0
Chris Gray <combly123@gmail.com>
2018-03-23 18:40:58 -0400

    Done with these files
    Make up the logged in and not logged in pages, as well as handling account creation and logging in

---
+++ index.php
@@ -0,0 +1,393 @@
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Movies for You</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="https://image.flaticon.com/icons/png/512/184/184578.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <link href= "css/movie.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: BizPage
+    Theme URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">Movies For You</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Home</a></li>
+          <li><a href="#movie">Movies</a></li>
+          <li class="menu-has-children"><a href="">My Account</a>
+            <ul>
+              <li><a href="#">Profile</a></li>
+              <li><a href="#">My Movies</a></li>
+            </ul>
+          </li> 
+          <li><a href="#login">Login</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+<?php
+
+function DBLogin()
+{
+  $host = "localhost";
+  $user = 'root';
+  $pass = '';
+  $db = 'moviedb';
+
+  $db = new mysqli($host, $user, $pass, $db) or die("Unable to connect");
+  if ($db->connect_error) {die("Connection failed: " . $db->connect_error);}
+  return $db;
+}
+// user is logged out
+ session_unset();
+ session_destroy();
+ 
+?>
+  <section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <div class="carousel-item active">
+            <div class="carousel-background"><img src="img/MovieSlide.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>MOVIES MOVIES MOVIES</h2>
+                <p> Find the BEST movies here</p>
+                <a href="#login" class="btn-get-started scrollto">Login</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="carousel-background"><img src="img/TammySlide.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>Tammy and the Dinosaur</h2>
+                <p>This Weeks Special Feature Picture</p>
+                <a href="#movie" class="btn-get-started scrollto">Browse Movies</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- #intro -->
+
+  <main id="main">
+
+    <!--==========================
+      Movie Section
+    ============================-->
+    <section id="movie"  class="section-bg" >
+      <div class="container">
+
+        <header class="section-header">
+          <br><br>
+          <h3 class="section-title">Our Movies</h3>
+        </header>
+<?php
+/* iterate through and select names and emails */
+$db = DBLogin();
+$sql = "SELECT title, runtime, plot, production, rating from movie";
+$result = $db->query($sql);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table class='t1'>
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Runtime</th>
+                <th>Plot</th>
+                <th>Production</th>
+                <th>Rating</th>
+            </tr>
+            </thead>
+          ";
+    while($row = $result->fetch_assoc()) {
+      echo "<tbody>";
+      echo "<tr>";
+      echo "<td>" . $row["title"] . "</td>";
+      echo "<td>" . $row["runtime"] . "</td>";
+      echo "<td>" . $row["plot"] . "</td>";
+      echo "<td>" . $row["production"] . "</td>";
+      echo "<td>" . $row["rating"] . "</td>";
+      echo "</tr>";
+      echo "</tbody>";
+      }
+    echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+  echo "
+    <form name = 'showings' action='movieShowings.php' method='POST' enctype='multipart/form-data'>
+    <br>
+    <h4> Find showings near you!</h4>";
+  $sql = "SELECT cname, city from complex";
+  $result = $db->query($sql);
+  echo "<select name='theatreComplex' required='required'>";
+    while($row = $result->fetch_assoc()) {
+      echo "<option value = '" . $row["cname"] . ", " . $row["city"] . "''>" . $row["cname"] . ", " . $row["city"] . "</option>";
+    }
+    echo "</select>";
+    echo "<form action='http://52.60.89.137/14aad4/database/A3redirect.html'>
+      <input type='submit' value='GO!' />
+  </form>";
+  $db->close();
+?>
+
+</div>
+</section>
+
+    <section id="testimonials" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <header class="section-header">
+          <h3>Movie Reviews</h3>
+        </header>
+
+        <div class="owl-carousel testimonials-carousel">
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-1.jpg" class="testimonial-img" alt="">
+            <h3>Saul Goodman</h3>
+            <h4>Ceo &amp; Founder</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+              Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-2.jpg" class="testimonial-img" alt="">
+            <h3>Sara Wilsson</h3>
+            <h4>Designer</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+              Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-3.jpg" class="testimonial-img" alt="">
+            <h3>Jena Karlis</h3>
+            <h4>Store Owner</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+              Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-4.jpg" class="testimonial-img" alt="">
+            <h3>Matt Brandon</h3>
+            <h4>Freelancer</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+              Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+          <div class="testimonial-item">
+            <img src="img/testimonial-5.jpg" class="testimonial-img" alt="">
+            <h3>John Larson</h3>
+            <h4>Entrepreneur</h4>
+            <p>
+              <img src="img/quote-sign-left.png" class="quote-sign-left" alt="">
+              Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+              <img src="img/quote-sign-right.png" class="quote-sign-right" alt="">
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #testimonials -->
+
+
+    <!--==========================
+      Login Section
+    ============================-->
+    <section id="login" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <div class="section-header">
+          <h3>Login</h3>
+          <p>Please log into your account here</p>
+        </div>
+
+      <!-- contact info -->
+        <div class="form">
+          <form action="login.php" method="post">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <input type="text" name="accountNumber" class="form-control" id="accountNumber" placeholder="accountNumber" required />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <input type="password" class="form-control" name="password" id="password" placeholder="password" required/>
+                <div class="validation"></div>
+              </div>
+            <div class="text-center"><button type="submit">Login</button></div>
+            <br>
+          </form>
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+
+     <section id="login" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <div class="section-header">
+          <h3>Create An Account</h3>
+          <p>Please make your account here</p>
+        </div>
+
+      <!-- contact info -->
+        <div class="form">
+          <form action="createAccount.php" method="post">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <input type="number" name="accountNumber" class="form-control" id="accountNumber" placeholder="accountNumber" required />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <input type="password" class="form-control" name="password" id="password" placeholder="password" required/>
+                <div class="validation"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <input type="text" class="form-control" name="fname" id="fname" placeholder="First Name" required/>
+                <div class="validation"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <input type="text" class="form-control" name="lname" id="lname" placeholder="Last Name" required/>
+                <div class="validation"></div>
+              </div>
+              <div class="form-group col-md-6">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" required/>
+                <div class="validation"></div>
+              </div>
+            <div class="text-center"><button type="submit">Create Account</button></div>
+            <br>
+          </form>
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
+        -->
+        Best <a href="https://bootstrapmade.com/">Bootstrap Templates</a> by BootstrapMade
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
