ca29cec775ddd41c8e7c8b268a86ab63507e68a6
Chris Gray <combly123@gmail.com>
2018-03-26 13:10:19 -0400

    delete member fix

--- deleteMember.php
+++ deleteMember.php
@@ -3,7 +3,43 @@
 session_start();
 include "dbLogin.php";
 $db = DBLogin();
-$sql ="DELETE FROM customer where AccountNumber = {$_POST['member']}";
+$member = $_POST['member'];
+// delete reviews and movies
+
+$sql ="DELETE FROM review where AccountNumber = {$member}";
+$db -> query($sql);
+$sql ="SELECT * FROM reservation where AccountNumber = {$member}";
+
+$result = $db->query($sql);
+    if ($result->num_rows > 0) 
+    {
+      // output data of each row
+      $row = $result->fetch_assoc();
+      print_r($row);
+      $id = $row['AccountNumber'];
+      $complex = $row['Complex']; 
+      $theatre = $row['Theatre'];
+      $time = $row['StartTime'];
+      $day = $row['Day'];
+      $numTickets = $row['NumTickets'];
+      $sql = "SELECT NumSeats FROM showing where Complex = '{$complex}' AND Theatre = '{$theatre}' AND StartTime = '{$time}' AND Day = '{$day}' ";
+      $res = $db -> query($sql);
+      if ($res->num_rows > 0) 
+      {
+      	// output data of each row
+      	$row = $res->fetch_assoc();
+      	$oldSeats = $row['NumSeats'];
+      	$newSeats = $oldSeats + $numTickets;
+      	$sql = "UPDATE showing SET numseats='$newSeats' where Complex = '{$complex}' AND Theatre = '{$theatre}' AND StartTime = '{$time}' AND Day = '{$day}' ";
+      	$db -> query($sql);
+      }
+      $sql = "DELETE FROM reservation where AccountNumber = {$member}";
+      $db -> query($sql);
+    }
+
+
+$sql ="DELETE FROM customer where AccountNumber = {$member}";
+echo "$sql";
 
 if($db->query($sql)) // will return true if succefull else it will return false
 {
